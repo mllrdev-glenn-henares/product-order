c8c5b3a53d9ea046b06d2887e5a5a67c27559670
Glenn Henares II <glenn.henares@mllrdev.com>
2021-08-03 10:46:00 +0800

    Create login page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,36 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import { RouteNames } from "@/core/enums/route-names";
+
+import Home from "@/pages/Home.vue";
+import Login from "@/pages/Login.vue";
+import Register from "@/pages/Register.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: "/",
+    redirect: "/home",
   },
   {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  }
-]
+    path: "/home",
+    name: RouteNames.HOME,
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: RouteNames.LOGIN,
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: RouteNames.REGISTER,
+    component: Register,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
