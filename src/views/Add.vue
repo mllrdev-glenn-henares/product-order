0955ee2c42b826a325b552eb1a55b253afec80d2
mllrdev-jerry-bayoneta <msd.jerry.bayoneta@gmail.com>
2021-08-11 12:45:34 +0800

    few fix

--- src/views/Add.vue
+++ src/views/Add.vue
@@ -1,23 +1,30 @@
 <template>
   <Header/>
   <ion-content>
-    <div class="createHeader">
-      <ion-item>
-        <ion-label>Supplier</ion-label>
-        <ion-input type="text"></ion-input>
-      </ion-item>
-      <ion-item>
-        <ion-label>PO description</ion-label>
-        <ion-input type="text"></ion-input>
-      </ion-item>
-      <ion-item>
-        <ion-label>Purchase Date</ion-label>
-        <ion-input type="text"></ion-input>
-      </ion-item>
-      <ion-item>
-        <ion-label>Requestor</ion-label>
-        <ion-input type="text"></ion-input>
-      </ion-item>
+    <div class="mainCreate">
+      <h1>Purchase Order</h1>
+      <h1>PO-0003</h1>
+      <div class="headDetail">
+        <ion-item>
+          <ion-label>Supplier</ion-label>
+          <ion-input type="text"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>PO description</ion-label>
+          <ion-input type="text"></ion-input>
+        </ion-item>
+      </div>
+      <div class="headDetail">
+        <ion-item>
+          <ion-label>Purchase Date</ion-label>
+          <ion-input type="date"></ion-input>
+        </ion-item>
+        <ion-item>
+          <ion-label>Requestor</ion-label>
+          <ion-input type="text"></ion-input>
+        </ion-item>
+      </div>
+      <hr>
     </div>
   </ion-content>
 </template>
@@ -49,35 +56,45 @@ export default defineComponent({
 </script>
 
 <style scoped>
-*{
+/* *{
   outline: solid yellow 1px;
-}
+} */
   h1{
-    display: inline-block;
-    margin-left: 2%;
- 
+    display: block;
+    margin: 1%;
+
   }
-  .createHeader{
+  .mainCreate {
+    padding: 1%;
     width: 70%;
     margin: 3% 15% 3%;
     color: grey;
     background-color: white;
 
   }
-  p{
+  p {
     color: white;
   }
   ion-label {
-    width: 40%;
+    width: 35%;
     padding: 2%;
     margin-left: 2%;
   }
   ion-input {
-    min-width: 10%;
-    margin: 0% 2%;
+
+    --padding-bottom: 8px;
   }
   ion-item {
     display: block;
+  }
+  .headDetail {
+    display: inline-block;
+    width: 50%;
+  }
+  hr {
+    margin: 0% -1%;
+    border-bottom: grey solid;
+    opacity: 50%;
   }
 
 </style>
