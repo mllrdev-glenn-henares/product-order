b9fe9958d6c9cd20b124f59f86c78d36dc2c993e
mllrdev-jerry-bayoneta <msd.jerry.bayoneta@gmail.com>
2021-09-21 23:56:48 +0800

    fix:imported ionic/vue to remove console error

--- src/shared/components/OrderList.vue
+++ src/shared/components/OrderList.vue
@@ -15,12 +15,22 @@
 
 <script lang="ts">
 import { defineComponent, PropType, computed } from "vue";
+import {
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/vue";
 import moment from "moment";
 import router from "@/router";
 import IOrderSimple from "@/core/interfaces/order/order-simple.interface";
 
 export default defineComponent({
   name: "order-list",
+  components:{
+    IonGrid,
+    IonRow,
+    IonCol,
+  },
   props: {
     orders: {
       required: true,
@@ -69,39 +79,73 @@ export default defineComponent({
 </script>
 
 <style scoped>
-h1 {
-  display: inline-block;
-  margin-left: 2%;
-}
-ion-title {
-  display: inline-block;
-}
-.tableTitle {
-  background-color: aqua;
-  color: black;
-  font-weight: bold;
-  margin-left: 5%;
-  margin-right: 5%;
-}
-ion-grid {
-  text-align: center;
-  color: black;
-  margin: 2%;
-}
-ion-col {
-  outline: 1px solid black;
-}
-#signInButton {
-  float: right;
-  margin-top: 13px;
-  margin-right: 2%;
-}
-#addButton {
-  float: right;
-  vertical-align: top;
-  margin-right: 2%;
-}
-.filterButton {
-  margin-left: 5%;
-}
+  h1{
+    display: inline-block;
+    margin-left: 2%;
+ 
+  }
+  ion-title{
+    display: inline-block;
+  }
+  .tableTitle{
+    background-color: aqua;
+    color: black;
+    font-weight: bold;
+    margin-left: 5%;
+    margin-right: 5%;
+  }
+  ion-grid{
+    padding: 20px;
+    text-align: center;
+    color: #878a8f;
+    background-color: #ffffff;
+    font-size: 14px;
+  }
+  ion-col{
+   text-align: left;
+   margin-bottom: 5px;
+  }
+  #signInButton{
+    float: right;
+    margin-top: 13px;
+    margin-right: 2%;
+  }
+  #addButton{
+    float: right;
+    vertical-align: top;
+    margin-right: 2%;
+  }
+  ion-row {
+    border-bottom: solid #f5f5f5 2px;
+    margin: 5px;
+    font-weight: 300;
+  }
+  .APPROVED {
+    text-align: center;
+    border-radius: 20px;
+    color: #72b2a4;
+    background-color: #c8eae5;
+    width: 10px;
+  }
+  .DENIED {
+    text-align: center;
+    border-radius: 20px;
+    color: #b75b69;
+    background-color: #eec7ce;
+    width: 10px;
+  }
+  .PENDING {
+    text-align: center;
+    border-radius: 20px;
+    color: #527ace;
+    background-color: #bdd0f6;
+    width: 10px;
+  }
+  .CLOSED {
+    text-align: center;
+    border-radius: 20px;
+    color: #846fa0;
+    background-color: #d0b8ef;
+    width: 10px;
+  }
 </style>
